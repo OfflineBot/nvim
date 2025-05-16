266f80f9e839f56c498abc7dd9e9a8c8d6f6725e
offlinebot <flintstonelg@gmail.com>
2025-05-16 03:03:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+My Neovim config with lazy.nvim mainly copy-pasted from [[https://github.com/cpow/neovim-for-newbs]] but customized.
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        serverity_sort = true,
+      })
+
+      vim.keymap.set("n", "m", function()
+        vim.diagnostic.open_float(nil, { focusable = false })
+      end)
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require("lspconfig")
+      --lspconfig.gopls.setup({
+      --  capabilities = capabilities
+      --})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.solargraph.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
